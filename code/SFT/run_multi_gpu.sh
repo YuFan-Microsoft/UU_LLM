9a4e4c65bd99f45e2706699bcaa0a06cbf239230
Yufan Zhao <yufzhao@microsoft.com>
2026-09-14 18:05:19 -0700

    Enforce LF endings for shell scripts

--- code/SFT/run_multi_gpu.sh
+++ code/SFT/run_multi_gpu.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+set -eu
+
 mkdir -p ./output/qwen3_5_4B_sft_chinese/
 deepspeed deepspeed_llm_trainer.py --dataset_name yufan/UltraData-SFT-2605-Chinese --model_name_or_path /yufan/open_source_models/Qwen3.5_VLM/Qwen3.5-4B/ \
    --max_seq_len 8192 --learning_rate 5e-6 --num_train_epochs 10 --gradient_checkpointing 1 --zero_stage 3 \
